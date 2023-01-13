883f50382a454a4467c5d4ffe9be1fe26ba4b5eb
charlesmenez <menezessscharles@gmail.com>
2023-01-13 20:11:34 -0300

    some keybinds change

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx = 1;    /* border pixel of windows */
-static const unsigned int gappx = 6; /* gaps between windows */
-static unsigned int snap = 32;       /* snap pixel */
-static int showbar = 1;              /* 0 means no bar */
-static int topbar = 1;               /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static unsigned int borderpx = 2;     /* border pixel of windows */
+static const unsigned int gappx = 20; /* gaps between windows */
+static unsigned int snap = 32;        /* snap pixel */
+static int showbar = 1;               /* 0 means no bar */
+static int topbar = 1;                /* 0 means bottom bar */
+static const char *fonts[] = {"monospace:size=8"};
+static const char dmenufont[] = "monospace:size=8";
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
 static char normfgcolor[] = "#bbbbbb";
@@ -93,8 +93,8 @@ ResourcePref resources[] = {
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_d, spawn, {.v = dmenucmd}},
+    {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
@@ -102,15 +102,15 @@ static const Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY, XK_space, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
+    {MODKEY | ShiftMask, XK_q, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
     {MODKEY, XK_z, setlayout, {.v = &layouts[3]}},
     {MODKEY | ControlMask, XK_x, setlayout, {.v = &layouts[4]}},
-    {MODKEY, XK_space, setlayout, {0}},
+    {MODKEY, XK_Escape, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
     {MODKEY, XK_0, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
@@ -120,7 +120,7 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+            TAGKEYS(XK_9, 8){MODKEY, XK_q, quit, {0}},
 };
 
 /* button definitions */
